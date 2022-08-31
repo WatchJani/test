f6b6be18ea3312f83234f3bfa4d2e53af7a3370a
WatchJani <jankokondic84@gmail.com>
2022-08-31 11:50:59 +0200

    radi

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express")
-
+require('dotenv').config({ path: './local.env' });
+const mongoose = require("mongoose")
 
 const app = express()
+
+
+
+const db = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+mongoose.connect(
+    db,
+    () => {
+        console.log('Connected');
+    },
+    (err) => console.log(err.message)
+);
 
 app.get("/", (req, res )=>{
     res.send("radi sve")
